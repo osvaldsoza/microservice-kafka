a1555c39e6c6c9a7097a03d059d77e1c036f3b98
osvaldsoza <osvald.soza@gmail.com>
2025-06-23 11:12:34 -0300

    created module order

---
+++ order/src/test/java/com/github/osvaldsoza/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.osvaldsoza.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
