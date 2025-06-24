e2a8680d5135ecbf526caceb4e2f81efe17bf043
osvaldsoza <osvald.soza@gmail.com>
2025-06-24 15:51:53 -0300

    api gateway implemented

--- order/src/main/java/com/github/osvaldsoza/order/controller/OrderController.java
+++ order/src/main/java/com/github/osvaldsoza/order/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.github.osvaldsoza.order.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
 
+    @GetMapping
+    public ResponseEntity<String> find(){
+        return ResponseEntity.ok().body("Hello");
+    }
 }
